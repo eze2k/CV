c7c550bbefc2807022c35fcf857693653a17f181
Ezedoska <larenga.eze2k@gmail.com>
2022-08-24 00:29:33 -0300

    shit be almost done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule
