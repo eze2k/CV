88b699a96a778a5bfb886f64572ee1c3b1bd0521
Ezedoska <larenga.eze2k@gmail.com>
2022-08-27 05:42:58 -0300

    nose

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
 import { InfoComponent } from './info/info.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HeroComponent,
-    InfoComponent
+    InfoComponent,
+    LoginComponent,
+    FooterComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent },
+      { path: 'registro', component: RegisterComponent },
+      { path: '', component: InfoComponent },
+      { path: '**', component: InfoComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
